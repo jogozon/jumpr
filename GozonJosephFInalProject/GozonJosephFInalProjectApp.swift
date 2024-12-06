2caeed56e2f756104ea1589206781bad1d21d357
Joseph Gozon <gozon@usc.edu>
2024-12-06 12:52:18 -0800

    Initial Commit

---
+++ GozonJosephFInalProject/GozonJosephFInalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GozonJosephFInalProjectApp.swift
+//  GozonJosephFInalProject
+//
+//  Created by Joseph Gozon on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GozonJosephFInalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
